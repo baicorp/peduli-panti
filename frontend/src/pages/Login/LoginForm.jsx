c0507adb4c92171738236244e343ae5f092e76e4
ahmds02 <ahmadsatriadi6@gmail.com>
2023-11-15 11:21:24 +0700

    pesan commit

--- frontend/src/pages/Login/LoginForm.jsx
+++ frontend/src/pages/Login/LoginForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import 'tailwindcss/tailwind.css';
 import logoGooglePlay from "../../assets/Icons/logo-google-play.png";
+import { baseStyle } from "../../assets/styles";
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
@@ -12,7 +13,7 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+    <div className={`${baseStyle}rounded-md shadow-sm -space-y-px justify-center items-center text-center ml-[37px]`}>
       <div className="max-w-md w-full space-y-8">
         <div>
           <h2 className='text-xl font-bold text-gray-900'>Ayo Bergabung Dengan Komunitas Kami!</h2>
@@ -37,7 +38,7 @@ const LoginPage = () => {
                 type="email"
                 autoComplete="email"
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                className="mb-[15px]  rounded-[8px] relative block w-[400px] h-[35px] px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Email address"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
@@ -53,7 +54,7 @@ const LoginPage = () => {
                 type="password"
                 autoComplete="current-password"
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                className="mb-[15px]  rounded-[8px] relative block w-[400px] h-[35px] px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
@@ -61,51 +62,16 @@ const LoginPage = () => {
             </div>
           </div>
 
-          <div className="flex items-center justify-between">
-            <div className="flex items-center">
-              <input
-                id="remember_me"
-                name="remember_me"
-                type="checkbox"
-                className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-                checked={rememberMe}
-                onChange={(e) => setRememberMe(e.target.checked)}
-              />
-              <label htmlFor="remember_me" className="ml-2 block text-sm text-gray-900">
-                Ingat Saya
-              </label>
-            </div>
-
-            <div className="text-sm">
-              <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                Lupa Kata Sandi
-              </a>
-            </div>
-          </div>
-
+     
           <div>
             <button
               type="submit"
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className=" group relative w-[400px] h-[50px] flex justify-center py-2 px-4 border border-transparent text-[24px]  rounded-md text-white bg-pink hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               Masuk
             </button>
           </div>
-          <div className='flex align-middle'>
-            <div className='bg-slate-400 h-px w-24 mt-2 me-3'></div>
-            <p className=" text-center text-sm leading-none font-normal text-slate-400">
-              Masuk menggunakan google
-            </p>
-            <div className='bg-slate-400 h-px w-24 mt-2 ms-3'></div>
-          </div>
-          <div className='flex justify-center'>
-            <button
-              type="submit"
-              className="group relative w-24 flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-              Google
-            </button>
-          </div>
+
           <div>
             <p className=" text-center text-sm leading-none font-normal text-slate-400">
               Belum memiliki akun? yuk daftar <span className='text-indigo-600 font-bold'>Disini</span>
@@ -114,7 +80,7 @@ const LoginPage = () => {
             <p className=" text-center text-sm leading-none font-normal text-slate-400 mt-3">
               Download Aplikasi
             </p>
-            <div className='flex justify-center mt-3'>
+            <div className='ml-[137px] mt-3 w-[120px] h-[79px]'>
               <img src={logoGooglePlay} alt="logo-google-play" />
             </div>
           </div>
