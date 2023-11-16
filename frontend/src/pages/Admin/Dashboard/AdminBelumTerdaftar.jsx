3cba3e76e82bd85f39b15ddbd4a8e0e307f16e3f
baicorp <bagusatoki@gmail.com>
2023-11-16 10:56:25 +0700

    add 'profile panti terdaftar' page

--- frontend/src/pages/Admin/Dashboard/AdminBelumTerdaftar.jsx
+++ frontend/src/pages/Admin/Dashboard/AdminBelumTerdaftar.jsx
@@ -1,5 +1,317 @@
 import React from "react";
 
 export default function AdminBelumTerdaftar() {
-  return <div>AdminBelumTerdaftar</div>;
+  return (
+    <div>
+      <h1 className="text-lg font-semibold px-9 py-6">
+        Silahkan Isi Profil dan Data Panti
+      </h1>
+      <hr className="text-border-color" />
+      <div className="px-9 py-6">
+        <div className="flex flex-col gap-4">
+          <div className="flex justify-between gap-3">
+            <label
+              htmlFor="namaPanti"
+              className="w-full max-w-[192px] font-semibold"
+            >
+              Nama Panti
+            </label>
+            <input
+              type="text"
+              id="namaPanti"
+              className="border rounded-md px-3 py-1 outline-none w-full"
+            />
+          </div>
+          <div className="flex justify-between gap-3">
+            <label
+              htmlFor="noTlpPanti"
+              className="w-full max-w-[192px] font-semibold"
+            >
+              No Tlp Panti
+            </label>
+            <input
+              type="number"
+              id="noTlpPanti"
+              className="border rounded-md px-3 py-1 outline-none w-full"
+            />
+          </div>
+          <div className="flex justify-between gap-3">
+            <label
+              htmlFor="namaPemilik"
+              className="w-full max-w-[192px] font-semibold"
+            >
+              Nama Pemilik
+            </label>
+            <input
+              type="text"
+              id="namaPemilik"
+              className="border rounded-md px-3 py-1 outline-none w-full"
+            />
+          </div>
+          <div className="flex justify-between gap-3">
+            <label
+              htmlFor="noTlpPemilik"
+              className="w-full max-w-[192px] font-semibold"
+            >
+              Nama Tlp Pemilik Panti
+            </label>
+            <input
+              type="number"
+              id="noTlpPemilik"
+              className="border rounded-md px-3 py-1 outline-none w-full"
+            />
+          </div>
+          <div className="flex flex-col gap-[14px]">
+            <label
+              htmlFor="deskripsiPanti"
+              className="w-full max-w-[192px] font-semibold"
+            >
+              Deskripsi Panti
+            </label>
+            <textarea
+              name="deskripsiPanti"
+              id="deskripsiPanti"
+              cols="30"
+              rows="7"
+              className="border rounded-md px-3 py-1 outline-none"
+              placeholder="Isi artikel yang akan kamu unggah"
+            ></textarea>
+          </div>
+          <div className="flex gap-[14px]">
+            <label
+              htmlFor="alamat"
+              className="w-full max-w-[192px] font-semibold"
+            >
+              Alamat
+            </label>
+            <input
+              type="text"
+              id="alamat"
+              className="border rounded-md px-3 py-1 outline-none w-full"
+            />
+          </div>
+          <div className="flex justify-between gap-3">
+            <label
+              htmlFor="provinsi"
+              className="w-full max-w-[192px] font-semibold"
+            >
+              Provinsi
+            </label>
+            <select
+              id="provinsi"
+              name="provinsi"
+              className="w-full border rounded-md px-3 py-1 outline-none"
+            >
+              <option value="null">--Pilih Provinsi--</option>
+              <option value="jawaTimur">Jawa Timur</option>
+              <option value="jawaBarat">Jawa Barat</option>
+              <option value="jawatengah">Jawa Tengah</option>
+              <option value="papua">Papua</option>
+            </select>
+          </div>
+          <div className="flex justify-between gap-3">
+            <label
+              htmlFor="kabupaten"
+              className="w-full max-w-[192px] font-semibold"
+            >
+              Kabupaten / Kota
+            </label>
+            <input
+              type="text"
+              id="kabupaten"
+              className="border rounded-md px-3 py-1 outline-none w-full"
+            />
+          </div>
+          <div className="flex justify-between gap-3">
+            <label
+              htmlFor="kecamatan"
+              className="w-full max-w-[192px] font-semibold"
+            >
+              Kecamatan
+            </label>
+            <input
+              type="text"
+              id="kecamatan"
+              className="border rounded-md px-3 py-1 outline-none w-full"
+            />
+          </div>
+          <div className="flex flex-col gap-[14px]">
+            <label
+              htmlFor="deskripsiKebutuhan"
+              className="w-full max-w-[192px] font-semibold"
+            >
+              Deskripsi Kebutuhan
+            </label>
+            <textarea
+              name="deskripsiKebutuhan"
+              id="deskripsiKebutuhan"
+              cols="30"
+              rows="7"
+              className="border rounded-md px-3 py-1 outline-none"
+              placeholder="Isi artikel yang akan kamu unggah"
+            ></textarea>
+          </div>
+          <div className="flex justify-between gap-3">
+            <label
+              htmlFor="programPanti"
+              className="w-full max-w-[192px] font-semibold"
+            >
+              Program Panti
+            </label>
+            <input
+              type="text"
+              id="programPanti"
+              className="border rounded-md px-3 py-1 outline-none w-full"
+            />
+          </div>
+          <div className="flex justify-between gap-3">
+            <label
+              htmlFor="jumlahPengurus"
+              className="w-full max-w-[192px] font-semibold"
+            >
+              Jumlah Pengurus
+            </label>
+            <input
+              type="number"
+              id="jumlahPengurus"
+              className="border rounded-md px-3 py-1 outline-none w-full"
+            />
+          </div>
+          <div className="flex justify-between gap-3">
+            <label
+              htmlFor="jumlahAnakLaki"
+              className="w-full max-w-[192px] font-semibold"
+            >
+              Jumlah Anak Laki-laki
+            </label>
+            <input
+              type="number"
+              id="jumlahAnakLaki"
+              className="border rounded-md px-3 py-1 outline-none w-full"
+            />
+          </div>
+          <div className="flex justify-between gap-3">
+            <label
+              htmlFor="jumlahAnakPerempuan"
+              className="w-full max-w-[192px] font-semibold"
+            >
+              Jumlah Anak Perempuan
+            </label>
+            <input
+              type="number"
+              id="jumlahAnakPerempuan"
+              className="border rounded-md px-3 py-1 outline-none w-full"
+            />
+          </div>
+          <div className="flex flex-col gap-[14px]">
+            <label
+              htmlFor="logo"
+              className="w-full max-w-[192px] font-semibold"
+            >
+              Logo Panti
+            </label>
+            <div className="flex gap-5">
+              <svg
+                viewBox="0 0 256 256"
+                className="w-24 h-24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect fill="none" />
+                <rect
+                  fill="none"
+                  height="160"
+                  rx="8"
+                  stroke="#000"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="16"
+                  width="192"
+                  x="32"
+                  y="48"
+                />
+                <path
+                  d="M32,168l50.3-50.3a8,8,0,0,1,11.4,0l44.6,44.6a8,8,0,0,0,11.4,0l20.6-20.6a8,8,0,0,1,11.4,0L224,184"
+                  fill="none"
+                  stroke="#000"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="16"
+                />
+                <circle cx="156" cy="100" r="12" />
+              </svg>
+              <div>
+                <p>
+                  Max : 2MB. Ukuran 200 x 60 pixel.
+                  <br />
+                  Format : jpg | png
+                </p>
+                <input
+                  type="file"
+                  accept="image/png, image/jpeg, image/jpg"
+                  id="logo"
+                  className="mt-2"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-[14px]">
+            <label
+              htmlFor="foto"
+              className="w-full max-w-[192px] font-semibold"
+            >
+              Foto Panti
+            </label>
+            <div className="flex gap-5">
+              <svg
+                viewBox="0 0 256 256"
+                className="w-24 h-24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect fill="none" />
+                <rect
+                  fill="none"
+                  height="160"
+                  rx="8"
+                  stroke="#000"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="16"
+                  width="192"
+                  x="32"
+                  y="48"
+                />
+                <path
+                  d="M32,168l50.3-50.3a8,8,0,0,1,11.4,0l44.6,44.6a8,8,0,0,0,11.4,0l20.6-20.6a8,8,0,0,1,11.4,0L224,184"
+                  fill="none"
+                  stroke="#000"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="16"
+                />
+                <circle cx="156" cy="100" r="12" />
+              </svg>
+              <div>
+                <p>
+                  Max : 2MB. Ukuran 200 x 60 pixel.
+                  <br />
+                  Format : jpg | png
+                </p>
+                <input
+                  type="file"
+                  accept="image/png, image/jpeg, image/jpg"
+                  id="foto"
+                  className="mt-2"
+                />
+              </div>
+            </div>
+          </div>
+          <input
+            type="submit"
+            value="Simpan"
+            className="bg-medium-pink px-4 py-2 rounded-md font-semibold mt-[18px] w-fit self-center"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
--- frontend/src/pages/Admin/Dashboard/AdminTerdaftar.jsx
+++ frontend/src/pages/Admin/Dashboard/AdminTerdaftar.jsx
@@ -1,317 +1,142 @@
 import React from "react";
+import fotoProfilePanti from "../../../assets/Images/foto-profile-panti.jpg";
 
 export default function AdminTerdaftar() {
   return (
-    <div>
-      <h1 className="text-lg font-semibold px-9 py-6">
-        Silahkan Isi Profil dan Data Panti
-      </h1>
-      <hr className="text-border-color" />
-      <div className="px-9 py-6">
-        <div className="flex flex-col gap-4">
-          <div className="flex justify-between gap-3">
-            <label
-              htmlFor="namaPanti"
-              className="w-full max-w-[192px] font-semibold"
-            >
-              Nama Panti
-            </label>
-            <input
-              type="text"
-              id="namaPanti"
-              className="border rounded-md px-3 py-1 outline-none w-full"
-            />
-          </div>
-          <div className="flex justify-between gap-3">
-            <label
-              htmlFor="noTlpPanti"
-              className="w-full max-w-[192px] font-semibold"
-            >
-              No Tlp Panti
-            </label>
-            <input
-              type="number"
-              id="noTlpPanti"
-              className="border rounded-md px-3 py-1 outline-none w-full"
-            />
-          </div>
-          <div className="flex justify-between gap-3">
-            <label
-              htmlFor="namaPemilik"
-              className="w-full max-w-[192px] font-semibold"
-            >
-              Nama Pemilik
-            </label>
-            <input
-              type="text"
-              id="namaPemilik"
-              className="border rounded-md px-3 py-1 outline-none w-full"
-            />
-          </div>
-          <div className="flex justify-between gap-3">
-            <label
-              htmlFor="noTlpPemilik"
-              className="w-full max-w-[192px] font-semibold"
-            >
-              Nama Tlp Pemilik Panti
-            </label>
-            <input
-              type="number"
-              id="noTlpPemilik"
-              className="border rounded-md px-3 py-1 outline-none w-full"
-            />
-          </div>
-          <div className="flex flex-col gap-[14px]">
-            <label
-              htmlFor="deskripsiPanti"
-              className="w-full max-w-[192px] font-semibold"
-            >
-              Deskripsi Panti
-            </label>
-            <textarea
-              name="deskripsiPanti"
-              id="deskripsiPanti"
-              cols="30"
-              rows="7"
-              className="border rounded-md px-3 py-1 outline-none"
-              placeholder="Isi artikel yang akan kamu unggah"
-            ></textarea>
-          </div>
-          <div className="flex gap-[14px]">
-            <label
-              htmlFor="alamat"
-              className="w-full max-w-[192px] font-semibold"
-            >
-              Alamat
-            </label>
-            <input
-              type="text"
-              id="alamat"
-              className="border rounded-md px-3 py-1 outline-none w-full"
-            />
-          </div>
-          <div className="flex justify-between gap-3">
-            <label
-              htmlFor="provinsi"
-              className="w-full max-w-[192px] font-semibold"
-            >
-              Provinsi
-            </label>
-            <select
-              id="provinsi"
-              name="provinsi"
-              className="w-full border rounded-md px-3 py-1 outline-none"
-            >
-              <option value="null">--Pilih Provinsi--</option>
-              <option value="jawaTimur">Jawa Timur</option>
-              <option value="jawaBarat">Jawa Barat</option>
-              <option value="jawatengah">Jawa Tengah</option>
-              <option value="papua">Papua</option>
-            </select>
-          </div>
-          <div className="flex justify-between gap-3">
-            <label
-              htmlFor="kabupaten"
-              className="w-full max-w-[192px] font-semibold"
-            >
-              Kabupaten / Kota
-            </label>
-            <input
-              type="text"
-              id="kabupaten"
-              className="border rounded-md px-3 py-1 outline-none w-full"
-            />
-          </div>
-          <div className="flex justify-between gap-3">
-            <label
-              htmlFor="kecamatan"
-              className="w-full max-w-[192px] font-semibold"
-            >
-              Kecamatan
-            </label>
-            <input
-              type="text"
-              id="kecamatan"
-              className="border rounded-md px-3 py-1 outline-none w-full"
-            />
-          </div>
-          <div className="flex flex-col gap-[14px]">
-            <label
-              htmlFor="deskripsiKebutuhan"
-              className="w-full max-w-[192px] font-semibold"
-            >
-              Deskripsi Kebutuhan
-            </label>
-            <textarea
-              name="deskripsiKebutuhan"
-              id="deskripsiKebutuhan"
-              cols="30"
-              rows="7"
-              className="border rounded-md px-3 py-1 outline-none"
-              placeholder="Isi artikel yang akan kamu unggah"
-            ></textarea>
-          </div>
-          <div className="flex justify-between gap-3">
-            <label
-              htmlFor="programPanti"
-              className="w-full max-w-[192px] font-semibold"
-            >
-              Program Panti
-            </label>
-            <input
-              type="text"
-              id="programPanti"
-              className="border rounded-md px-3 py-1 outline-none w-full"
-            />
-          </div>
-          <div className="flex justify-between gap-3">
-            <label
-              htmlFor="jumlahPengurus"
-              className="w-full max-w-[192px] font-semibold"
-            >
-              Jumlah Pengurus
-            </label>
-            <input
-              type="number"
-              id="jumlahPengurus"
-              className="border rounded-md px-3 py-1 outline-none w-full"
-            />
-          </div>
-          <div className="flex justify-between gap-3">
-            <label
-              htmlFor="jumlahAnakLaki"
-              className="w-full max-w-[192px] font-semibold"
-            >
-              Jumlah Anak Laki-laki
-            </label>
-            <input
-              type="number"
-              id="jumlahAnakLaki"
-              className="border rounded-md px-3 py-1 outline-none w-full"
-            />
-          </div>
-          <div className="flex justify-between gap-3">
-            <label
-              htmlFor="jumlahAnakPerempuan"
-              className="w-full max-w-[192px] font-semibold"
-            >
-              Jumlah Anak Perempuan
-            </label>
-            <input
-              type="number"
-              id="jumlahAnakPerempuan"
-              className="border rounded-md px-3 py-1 outline-none w-full"
-            />
-          </div>
-          <div className="flex flex-col gap-[14px]">
-            <label
-              htmlFor="logo"
-              className="w-full max-w-[192px] font-semibold"
-            >
-              Logo Panti
-            </label>
-            <div className="flex gap-5">
-              <svg
-                viewBox="0 0 256 256"
-                className="w-24 h-24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect fill="none" />
-                <rect
-                  fill="none"
-                  height="160"
-                  rx="8"
-                  stroke="#000"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="16"
-                  width="192"
-                  x="32"
-                  y="48"
-                />
-                <path
-                  d="M32,168l50.3-50.3a8,8,0,0,1,11.4,0l44.6,44.6a8,8,0,0,0,11.4,0l20.6-20.6a8,8,0,0,1,11.4,0L224,184"
-                  fill="none"
-                  stroke="#000"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="16"
-                />
-                <circle cx="156" cy="100" r="12" />
-              </svg>
-              <div>
-                <p>
-                  Max : 2MB. Ukuran 200 x 60 pixel.
-                  <br />
-                  Format : jpg | png
-                </p>
-                <input
-                  type="file"
-                  accept="image/png, image/jpeg, image/jpg"
-                  id="logo"
-                  className="mt-2"
-                />
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-col gap-[14px]">
-            <label
-              htmlFor="foto"
-              className="w-full max-w-[192px] font-semibold"
-            >
-              Foto Panti
-            </label>
-            <div className="flex gap-5">
-              <svg
-                viewBox="0 0 256 256"
-                className="w-24 h-24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect fill="none" />
-                <rect
-                  fill="none"
-                  height="160"
-                  rx="8"
-                  stroke="#000"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="16"
-                  width="192"
-                  x="32"
-                  y="48"
-                />
-                <path
-                  d="M32,168l50.3-50.3a8,8,0,0,1,11.4,0l44.6,44.6a8,8,0,0,0,11.4,0l20.6-20.6a8,8,0,0,1,11.4,0L224,184"
-                  fill="none"
-                  stroke="#000"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="16"
-                />
-                <circle cx="156" cy="100" r="12" />
-              </svg>
-              <div>
-                <p>
-                  Max : 2MB. Ukuran 200 x 60 pixel.
-                  <br />
-                  Format : jpg | png
-                </p>
-                <input
-                  type="file"
-                  accept="image/png, image/jpeg, image/jpg"
-                  id="foto"
-                  className="mt-2"
-                />
-              </div>
-            </div>
-          </div>
-          <input
-            type="submit"
-            value="Simpan"
-            className="bg-medium-pink px-4 py-2 rounded-md font-semibold mt-[18px] w-fit self-center"
-          />
-        </div>
-      </div>
+    <div className="px-9 py-6">
+      <img
+        src={fotoProfilePanti}
+        alt="foto profil panti"
+        className="rounded-lg mb-[22px] w-full object-cover"
+      />
+      <table className="border-collapse w-full">
+        <tr>
+          <th className="px-6 py-[18px] border border-border-color text-start">
+            Nama Panti
+          </th>
+          <td className="px-6 py-[18px] border border-border-color ">
+            Yayasan Islam Media Kasih
+          </td>
+        </tr>
+        <tr>
+          <th className="px-6 py-[18px] border border-border-color text-start ">
+            No Tlp Panti
+          </th>
+          <td className="px-6 py-[18px] border border-border-color">
+            0217328463
+          </td>
+        </tr>
+        <tr>
+          <th className="px-6 py-[18px] border border-border-color text-start ">
+            Nama Pengurus
+          </th>
+          <td className="px-6 py-[18px] border border-border-color">
+            Bani Agusalam
+          </td>
+        </tr>
+        <tr>
+          <th className="px-6 py-[18px] border border-border-color text-start ">
+            Nama Pemilik
+          </th>
+          <td className="px-6 py-[18px] border border-border-color">
+            Agus Salim
+          </td>
+        </tr>
+        <tr>
+          <th className="px-6 py-[18px] border border-border-color text-start ">
+            Nama Tlp Pemilik
+          </th>
+          <td className="px-6 py-[18px] border border-border-color">
+            089543246765
+          </td>
+        </tr>
+        <tr>
+          <th className="px-6 py-[18px] border border-border-color text-start ">
+            Deskripsi Panti
+          </th>
+          <td className="px-6 py-[18px] border border-border-color">-</td>
+        </tr>
+        <tr>
+          <th className="px-6 py-[18px] border border-border-color text-start ">
+            Alamat Panti
+          </th>
+          <td className="px-6 py-[18px] border border-border-color">
+            Cipadu Rt.001/004 No.13 Ciledug Tangerang
+          </td>
+        </tr>
+        <tr>
+          <th className="px-6 py-[18px] border border-border-color text-start ">
+            Provinsi
+          </th>
+          <td className="px-6 py-[18px] border border-border-color">
+            Jawa Barat
+          </td>
+        </tr>
+        <tr>
+          <th className="px-6 py-[18px] border border-border-color text-start ">
+            Kabupaten / Kota
+          </th>
+          <td className="px-6 py-[18px] border border-border-color">
+            Tangerang
+          </td>
+        </tr>
+        <tr>
+          <th className="px-6 py-[18px] border border-border-color text-start ">
+            Kecamatan
+          </th>
+          <td className="px-6 py-[18px] border border-border-color">Ciledug</td>
+        </tr>
+        <tr>
+          <th className="px-6 py-[18px] border border-border-color text-start ">
+            Program Panti
+          </th>
+          <td className="px-6 py-[18px] border border-border-color">-</td>
+        </tr>
+        <tr>
+          <th className="px-6 py-[18px] border border-border-color text-start ">
+            Deskripsi Program
+          </th>
+          <td className="px-6 py-[18px] border border-border-color">-</td>
+        </tr>
+        <tr>
+          <th className="px-6 py-[18px] border border-border-color text-start ">
+            Kebutuhan Panti
+          </th>
+          <td className="px-6 py-[18px] border border-border-color">
+            Yayasan Islam Media Kasih
+          </td>
+        </tr>
+        <tr>
+          <th className="px-6 py-[18px] border border-border-color text-start ">
+            Deskripsi Kebutuhan
+          </th>
+          <td className="px-6 py-[18px] border border-border-color">-</td>
+        </tr>
+        <tr>
+          <th className="px-6 py-[18px] border border-border-color text-start ">
+            Jumlah Pengurus
+          </th>
+          <td className="px-6 py-[18px] border border-border-color">15</td>
+        </tr>
+        <tr>
+          <th className="px-6 py-[18px] border border-border-color text-start ">
+            Jumlah Anak Laki-laki
+          </th>
+          <td className="px-6 py-[18px] border border-border-color">20</td>
+        </tr>
+        <tr>
+          <th className="px-6 py-[18px] border border-border-color text-start ">
+            Jumlah Anak Perempuan
+          </th>
+          <td className="px-6 py-[18px] border border-border-color">30</td>
+        </tr>
+        <tr>
+          <th className="px-6 py-[18px] border border-border-color text-start">
+            Jumlah Anak{" "}
+          </th>
+          <td className="px-6 py-[18px] border border-border-color">50</td>
+        </tr>
+      </table>
     </div>
   );
 }
